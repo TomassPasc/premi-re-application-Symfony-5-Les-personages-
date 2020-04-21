6ac51417f607f337298af549e2ffcf71521e66b4
ThomasPascual <thoma1@free.fr>
2020-04-21 22:07:38 +0200

    ajout de tous les persos sous forme de tableau

--- src/Controller/PersonnageController.php
+++ src/Controller/PersonnageController.php
@@ -21,14 +21,46 @@ class PersonnageController extends AbstractController
      */
     public function persos()
     {
-        return $this->render('personnage/persos.html.twig',[
-            "pseudo" => "toto",
-            "age" => 25,
+        $perso1 =  [
+            "pseudo" => "Marc",
+            "age" => 52,
+            "sexe" => true,
             "carac" => [
                 "force" => 3,
                 "agi" => 2,
                 "intel" => 3
-            ]
+                ]
+            ];
+        
+            $perso2 =  [
+                "pseudo" => "Milo",
+                "age" => 12,
+                "sexe" => true,
+                "carac" => [
+                    "force" => 6,
+                    "agi" => 7,
+                    "intel" => 1
+                    ]
+                ];
+
+            $perso3 =  [
+                "pseudo" => "Tya",
+                "age" => 25,
+                "sexe" => false,
+                "carac" => [
+                    "force" => 3,
+                    "agi" => 9,
+                    "intel" => 10
+                    ]
+                ];
+            
+            $players = [
+                "j1" => $perso1,
+                "j2" => $perso2,
+                "j3" => $perso3
+            ];
+        return $this->render('personnage/persos.html.twig',[
+            "players" => $players
         ]);
     }
 }
